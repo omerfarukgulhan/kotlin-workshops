273c0da0af2017c44b035c63cd36a24e9526cd50
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-09-18 17:36:10 +0300

    navigation added

---
+++ 05-navigation/app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigation.ui.theme.NavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "firstscreen") {
+        composable("firstscreen") {
+            FirstScreen { name ->
+                navController.navigate("secondscreen/$name")
+            }
+        }
+        composable("secondscreen/{name}") {
+            val name = it.arguments?.getString("name") ?: "no name"
+            SecondScreen(name) {
+                navController.navigate("firstscreen")
+            }
+        }
+    }
+}
---
+++ 05-navigation/app/src/main/java/com/example/navigation/SecondScreen.kt
@@ -0,0 +1,36 @@
+package com.example.navigation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SecondScreen(name: String, navigateToFirstScreen: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "This is the second screen")
+        Text(text = "Welcome ${name}")
+        Button(onClick = { navigateToFirstScreen() }) {
+            Text(text = "Go to first screen")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondPreview() {
+    SecondScreen("ofg", {})
+}
